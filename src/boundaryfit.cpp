5d4fa52c6dcf47b0066d89551472bbf8420f7186
Nicolás Cardiel <nicocardiel@users.noreply.github.com>
2009-01-09 17:29:16 +0000

    First version using boundary fits computed with the Fortran program BoundFit

--- src/boundaryfit.cpp
+++ src/boundaryfit.cpp
@@ -19,34 +19,115 @@
  */
 
 #include <iostream>
-#include <cmath>
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ios;
+
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+
 #include <vector>
-#include <algorithm>
+using std::vector;
+
+#include <stdlib.h>
+
 #include "genericpixel.h"
 
-using namespace std;
- 
 //-----------------------------------------------------------------------------
-//Calcula el boundary fit a los datos en el vector vec
+//Calcula un boundary fit, ajustando los datos en el vector vec. El resultado
+//del ajuste, evaluado en los mismos pixeles, se almacena en el vector fit.
+//Finalmente tambien se evalua el boundary fit en todos los pixeles contenidos
+//en el vector eval. De esta forma, este vector puede usarse para calcular un
+//unico valor en el centro de una banda, o todo un conjunto de valores.
 bool boundaryfit(vector <GenericPixel> &vec, const bool lerr,
                  vector <GenericPixel> &fit,
                  vector <GenericPixel> &eval)
 {
   //---------------------------------------------------------------------------
-  //valores retornados en caso de error o imposibilidad de calculo
-  //a rellenar
-  //a rellenar
-  //a rellenar
-
+  //protecciones
   long num=vec.size();
   if (num <= 0)
   {
     cout << "ERROR in function boundaryfit: vector size = 0" << endl;
     return(false);
   }
+  if (num != fit.size())
+  {
+    cout << "ERROR in function boundaryfit: size of vectors vec and fit "
+         << "does not match" << endl;
+    cout << "size(vec): " << num << endl;
+    cout << "size(fit): " << fit.size() << endl;
+    return(false);
+  }
 
-  //valores temporales
-  eval[0].setflux(1.0);
-  eval[0].seteflux(0.01);
+  //---------------------------------------------------------------------------
+  //generamos un fichero ASCII con los datos a ajustar
+  ofstream outdatafile("boundfit.dat",ios::out);
+  if(!outdatafile)
+  {
+    cerr << "File boundfit.dat could not be opened" << endl;
+    return(false);
+  }
+  for (long i=0; i<num; i++)
+  {
+    outdatafile << vec[i].getwave() << "   " << vec[i].getflux() << endl;
+  }
+  outdatafile.close();
+  //calculamos ajuste
+  system("source boundfit_pol.tcsh > boundfit_pol.log");
+  //leemos el ajuste
+  ifstream inputfitfile("boundfit.out",ios::in);
+  if(!inputfitfile)
+  {
+    cerr << "File boundfit.out could not be opened" << endl;
+    return(false);
+  }
+  for (long i=0; i<num; i++)
+  {
+    double tempwave,tempflux;
+    inputfitfile >> tempwave >> tempflux;
+    fit[i].setwave(tempwave);
+    fit[i].setflux(tempflux);
+    fit[i].seteflux(0.0);
+    fit[i].setpixelfraction(vec[i].getpixelfraction());
+  }
+  inputfitfile.close();
+  //leemos grado y coeficientes del ajuste polinomico
+  ifstream inputcoeff("boundfit.coeff",ios::in);
+  if(!inputcoeff)
+  {
+    cerr << "File boundfit.coeff could not be opened" << endl;
+    return(false);
+  }
+  long polydeg;
+  inputcoeff >> polydeg;
+  cout << "Polynomial degree: " << polydeg << endl;
+  double * polycoeff = new double [polydeg+1];
+  long idum;
+  for (long i=0; i<=polydeg; i++)
+  {
+    inputcoeff >> idum >> polycoeff[i];
+    cout << "Coeff#" << i << ": " << polycoeff[i] << endl;
+  }
+  inputcoeff.close();
+  //evaluamos el polinomio ajustado en los pixeles solicitados
+  long num_eval=eval.size();
+  if(num_eval > 0)
+  {
+    for (long i=0; i<num_eval; i++)
+    {
+      double tempwave=eval[i].getwave();
+      double tempflux=polycoeff[polydeg];
+      for (long k=polydeg; k>=1; k--)
+      {
+        tempflux=tempflux*tempwave+polycoeff[k-1];
+      }
+      eval[i].setflux(tempflux);
+      eval[i].seteflux(0.01);
+    }
+  }
+  delete [] polycoeff;
   return(true);
 }
