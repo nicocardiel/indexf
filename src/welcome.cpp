6a9d58100627022fd7340368b408632c7b0ee5e9
Sergio Pascual <sergiopasra@users.noreply.github.com>
2013-02-05 11:05:10 +0100

    Correct the proper definition of the macro HAVE_CPGPLOT_H.

--- src/welcome.cpp
+++ src/welcome.cpp
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
 #include <iostream>
 
 using namespace std;
@@ -7,5 +11,11 @@ void welcome()
   //la version esta definida como variable global
   extern const char *versionPtr;
 
-  cout << "#\n# Using indexf version: " << versionPtr << "\n#" << endl;
+  cout << "#\n# Using indexf version: " << versionPtr 
+#ifdef HAVE_CPGPLOT_H
+       << " (PGPLOT enabled)"
+#else
+       << " (PGPLOT disabled)"
+#endif /* HAVE_CPGPLOT_H */
+       << "\n#" << endl;
 }
