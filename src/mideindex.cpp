5fc439cc629624c49ef941c89f201d68a7fda5b0
Nicolas Cardiel <nicolas.cardiel@gmail.com>
2013-06-13 11:02:55 +0200

    Fix problem when bandpasses fall outside the available wavelength range and running the program from pyindexf: output meansn in each band equal to zero for all the bands prior returning from function.

--- src/mideindex.cpp
+++ src/mideindex.cpp
@@ -122,34 +122,50 @@ bool mideindex(const bool &lerr, const double *sp_data, const double *sp_error,
     if ( (c3[nb] < 1.0) || (c4[nb] > static_cast<double>(naxis1)) )
     {
       out_of_limits=true;          //indice fuera de limites: no se puede medir
-      return(false);
     }
-    j1[nb] = static_cast<long>(c3[nb]);         //band limit: integer (channel)
-    j2[nb] = static_cast<long>(c4[nb]);         //band limit: integer (channel)
-    d1[nb] = c3[nb]-static_cast<double>(j1[nb]);//fraction (excess left chan.)
-    d2[nb] = c4[nb]-static_cast<double>(j2[nb]);//fraction (defect right chan.)
-    rl[nb] = cb[nb]-ca[nb];                 //redshifted band width (angstroms)
-    rg[nb] = c4[nb]-c3[nb]+1;               //redshifted band width (channels)
-    if(pyindexf)
+    if (!out_of_limits)
+    {
+      j1[nb] = static_cast<long>(c3[nb]);       //band limit: integer (channel)
+      j2[nb] = static_cast<long>(c4[nb]);       //band limit: integer (channel)
+      d1[nb] = c3[nb]-static_cast<double>(j1[nb]);//fraction (excess left chan)
+      d2[nb] = c4[nb]-static_cast<double>(j2[nb]);//fraction (defect right chan)
+      rl[nb] = cb[nb]-ca[nb];               //redshifted band width (angstroms)
+      rg[nb] = c4[nb]-c3[nb]+1;             //redshifted band width (channels)
+    }
+  }
+  //calculamos S/N_A promedio en cada banda por separado (solo cuando
+  //ejecutamos el programa a traves de pyindexf y no se ha producido el error
+  //debido a que el indice cae fuera de limites)
+  if(pyindexf)
+  {
+    for (long nb=0; nb < nbands; nb++)
     {
       double meansn = 0;
       if(lerr)
       {
-        for (long j=j1[nb]; j <= j2[nb]+1; j++)
-        {
-          if (sp_error[j-1] <= 0)
+        if (!out_of_limits)
           {
-            negative_error=true;
-            return(false);
+          for (long j=j1[nb]; j <= j2[nb]+1; j++)
+          {
+            if (sp_error[j-1] <= 0)
+            {
+              negative_error=true;
+              return(false);
+            }
+            meansn+=sp_data[j-1]/sp_error[j-1];
           }
-          meansn+=sp_data[j-1]/sp_error[j-1];
+          meansn/=static_cast<double>(j2[nb]-j1[nb]+2);
+          meansn/=sqrt(cdelt1); //calculamos senal/ruido por angstrom
         }
-        meansn/=static_cast<double>(j2[nb]-j1[nb]+2);
-        meansn/=sqrt(cdelt1); //calculamos senal/ruido por angstrom
       }
       cout << "python> {'meansn" << setw(3) << setfill('0') << nb+1 << "': "
            <<           meansn << "}" << endl;
     }
+  }
+  // si el indice cae fuera de limites, no se puede medir
+  if (out_of_limits)
+  {
+    return(false);
   }
   //calculamos limites en j1 y j2, por si las bandas no estan en orden
   long j1min = j1[0];
