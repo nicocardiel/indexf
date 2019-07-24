545ec9f53e0a287ca98f87dc1239cfa0815137b3
Nicolás Cardiel <cardiel@ucm.es>
2019-07-24 14:26:37 +0200

    Change pngmath by imgmath in extensions

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinxcontrib.bibtex',
-              'sphinx.ext.pngmath']
+              'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
